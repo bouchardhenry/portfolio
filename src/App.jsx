c436f99164c4435817adc6b369b26263fb8806db
bouchardhenry <bouchardhenry@gmail.com>
2026-02-24 12:24:23 +0100

    added images + gifs

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import './App.css'
-import AppRouter from './Router'
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Contact from './pages/Contact/Contact'
+import Projects from './pages/Projects/Projects'
 
-function App() {
+export default function App() {
   return (
-    <>
-      <Navbar />
-      <main>
-        <AppRouter />
-      </main>
-      <Footer />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="projects" element={<Projects />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
