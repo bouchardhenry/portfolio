f0d0df0de4ee0aac440b5fb3932fd782a6712690
bouchardhenry <bouchardhenry@gmail.com>
2026-02-24 09:38:41 +0100

    base for pages + changed css files to module files

---
+++ src/pages/Projects/ProjectDetail/ProjectDetail.jsx
@@ -0,0 +1,9 @@
+import styles from './ProjectDetail.module.css'
+
+export default function ProjectDetail() {
+  return (
+    <div className={styles.projectDetail}>
+      <h1>Project Detail</h1>
+    </div>
+  )
+}
