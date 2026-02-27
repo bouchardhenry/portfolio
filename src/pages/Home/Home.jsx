bae5366e0d770a9e14d3abff06a2d07529bb9334
bouchardhenry <bouchardhenry@gmail.com>
2026-02-27 14:51:08 +0100

    no overscroll form index on mobile

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,21 @@
+import { useEffect } from 'react'
 import styles from './Home.module.css'
 import labyrinth from '../../assets/images/labyrint.gif'
 import nameSvg from '../../assets/images/HB_rightside.png'
 
 export default function Home() {
+  useEffect(() => {
+    const isMobile = window.innerWidth <= 1023
+    if (isMobile) {
+      document.body.style.overflow = 'hidden'
+      document.body.style.overscrollBehavior = 'none'
+    }
+    return () => {
+      document.body.style.overflow = ''
+      document.body.style.overscrollBehavior = ''
+    }
+  }, [])
+
   return (
     <div className={styles.page}>
 
